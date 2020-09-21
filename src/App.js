75e84c5a5c9a33526b93123bd94d579311fe8988
Aiva <aivaras.adukauskas@gmail.com>
2020-09-21 12:52:44 +0200

    Amazon clone

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+		<h1>Wot UP?</h1>
+		<h2>Web 3fauzand;]</h2>
+		{/* Wee need react-ROUTER to get the contents of e.g.: */}
+		{/* localhost/login */}
+		{/* localhost/checkout */}
+		{/* It's all on App component level */}
+    </div>
+  );
+}
+
+export default App;
